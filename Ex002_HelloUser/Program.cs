d11d208daf219928e1caee8b8131db49cd866664
tvkalex <tvkalex@yandex.ru>
2022-11-27 16:59:11 +0300

    Solved null mistake in string

--- Ex002_HelloUser/Program.cs
+++ Ex002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!; //Add ! as null forgiving to avoid mistake "Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL." [Ex002_HelloUser]csharp(CS8600)
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
 Console.Write("Привет, ");
