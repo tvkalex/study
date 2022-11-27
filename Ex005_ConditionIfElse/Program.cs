149cc62a004dc93afce2ae52811f86a2d2bf13d1
tvkalex <tvkalex@yandex.ru>
2022-11-27 19:17:51 +0300

    Add Ex005_ConditionIfElse

---
+++ Ex005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine()!; //Add ! as null forgiving to avoid mistake
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
