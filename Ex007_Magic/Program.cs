4293c8eb1c73c579bf712dfb3e9ba6899d47ae10
tvkalex <tvkalex@yandex.ru>
2022-11-29 15:24:50 +0300

    Changed WriteLine symbol

--- Ex007_Magic/Program.cs
+++ Ex007_Magic/Program.cs
@@ -17,19 +17,19 @@ int xa = 40, ya = 1,
     xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.WriteLine("*");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+Console.WriteLine("*");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.WriteLine("*");
 
 int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10000)
+while (count < 100000)
 {
     int what = new Random().Next(0, 3); // [0;3) 0 1 2
     if (what == 0)
@@ -48,6 +48,6 @@ while (count < 10000)
         y = (y + yc) / 2;
     }
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
+    Console.WriteLine("*");
     count = count + 1; //те же команды count +=1 или count++ 
 }
