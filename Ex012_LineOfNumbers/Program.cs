2e50609dc37a01a51c9eae7e44f4bb2467fa4608
tvkalex <tvkalex@yandex.ru>
2022-11-30 02:23:49 +0300

    Add Ex012_LineOfNumbers

--- Ex012_LineOfNumbers/Program.cs
+++ Ex012_LineOfNumbers/Program.cs
@@ -5,3 +5,44 @@
 2 -> " -2, -1, 0, 1, 2"
 */
 
+/*
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+int num_2 = -1 * num;
+while (num_2 != num + 1)
+{
+    System.Console.Write(num_2 + " ");
+    num_2++;
+}
+*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+for (int i = -N; i <= N; i++)
+    Console.Write(i + " ");
+
+/*
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+while (i<= N)
+{
+    Console.Write(i + " ");
+    i++;
+}
+*/
+
+/*
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+{
+    N = -N;
+}
+int i = -N;
+while (i<= N)
+{
+    Console.Write(i + " ");
+    i++;
+}
+*/
