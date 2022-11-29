54f7da1aa384078900331b91ecb2916d73b713b8
tvkalex <tvkalex@yandex.ru>
2022-11-30 01:37:57 +0300

    Add Ex011_WeekDays

--- Ex011_WeekDays/Program.cs
+++ Ex011_WeekDays/Program.cs
@@ -1,4 +1,10 @@
-﻿System.Console.WriteLine("Введите число: ");
+﻿/*
+Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+
+System.Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 switch (number)
 {
@@ -18,12 +24,49 @@ switch (number)
         System.Console.WriteLine("Это пятница");
         break;
     case 6:
-        System. Console.WriteLine("Это суббота");
+        System.Console.WriteLine("Это суббота");
         break;
     case 7:
-        System. Console.WriteLine("Это воскресенье");
+        System.Console.WriteLine("Это воскресенье");
         break;
     default:
-        System. Console. WriteLine ("А сколько дней в твоей неделе?");
+        System.Console.WriteLine("А сколько дней в твоей неделе?");
         break;
 }
+
+/*
+Console.WriteLine("Введите число: ");
+int num = Convert. ToInt32(Console. ReadLine ());
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine ("Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+*/
