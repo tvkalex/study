fa0a644ad46f4e436e62bb74d85274ec1e6816dd
tvkalex <tvkalex@yandex.ru>
2022-12-01 13:21:43 +0300

    Add Ex014_ArrayCreationMethod

--- Ex014_ArrayCreationMethod/Program.cs
+++ Ex014_ArrayCreationMethod/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
