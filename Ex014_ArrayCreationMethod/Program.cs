b50815c782ae32ee85e5d8a095c495f63561763a
tvkalex <tvkalex@yandex.ru>
2022-12-01 18:23:17 +0300

    Add Ex014_ArrayCreationMethod

--- Ex014_ArrayCreationMethod/Program.cs
+++ Ex014_ArrayCreationMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) //создание массива автозаполнения случайными числами
 {
     int length = collection.Length;
     int index = 0;
@@ -8,3 +8,40 @@
         index++;
     }
 }
+
+void PrintArray(int[] col) //создание массива вывода на экран
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) //создание массива поиска индекса элемента массива
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //-1 выводится для индикации ошибки в случае ввода числа, отсутствующего в массиве
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 8);
+Console.WriteLine(pos);
