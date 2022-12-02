8c0c502c953a430c3502ef6a7f30fe4f28b83e14
tvkalex <tvkalex@yandex.ru>
2022-12-03 01:20:46 +0300

    Add Ex016_RemoveSecondNumber solution

--- Ex016_RemoveSecondNumber/Program.cs
+++ Ex016_RemoveSecondNumber/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// int num = new Random().Next(100, 1000);
+// System.Console.WriteLine(num);
+// int num1 = num / 100;
+// int num2 = num / 10;
+// int num3 = num % 10;
+// System.Console.WriteLine($"{num1}{num3}");
+
+
+int num = new Random().Next(100, 1000);
+System.Console.WriteLine(num);
+System.Console.WriteLine((num / 100) * 10 + (num % 10));
+
+// int num = new Random().Next(100, 1000);
+// System.Console.WriteLine(num);
+// string chr = Convert.ToString(num);
+// System.Console.WriteLine($"{chr [0]}{chr [^1]}");
+
+
+// вывод первой и последней буквы слова. ^1 означает array.Length - 1
+// string word = "hello";
+// System.Console.WriteLine($"{word [0]}{word [^1]}"); 
