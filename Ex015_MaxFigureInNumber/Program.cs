5aa4ea96a1f081959b0799a5b0178997d7cc3399
tvkalex <tvkalex@yandex.ru>
2022-12-03 00:43:10 +0300

    Add Ex015_MaxFigureInNumber solution

--- Ex015_MaxFigureInNumber/Program.cs
+++ Ex015_MaxFigureInNumber/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+// Например:
+// • 78 -> 8
+// • 12-> 2
+// • 85 -> 8
+
+int num = new Random().Next(10, 100); // [10, 100) [10, 99]
+System.Console.WriteLine(num);
+int num1 = num / 10; // 78 / 10 = 7
+int num2 = num % 10; // 78 % 10 = 8
+if (num1 > num2)
+{
+    System.Console.WriteLine(num1);
+}
+else
+{
+    System.Console.WriteLine(num2);
+}
