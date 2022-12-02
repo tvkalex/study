4eed4250309206e196f9e2af10667171b62d42d0
tvkalex <tvkalex@yandex.ru>
2022-12-03 01:35:08 +0300

    Add Ex016_RemoveSecondNumber solution

--- Ex016_RemoveSecondNumber/Program.cs
+++ Ex016_RemoveSecondNumber/Program.cs
@@ -7,15 +7,23 @@
 // int num3 = num % 10;
 // System.Console.WriteLine($"{num1}{num3}");
 
+// int num = new Random().Next(100, 1000);
+// System.Console.WriteLine(num);
+// int num1 = num / 100;
+// int num2 = num % 10;
+// System.Console.WriteLine($"{num1}{num2}");
+
 
 int num = new Random().Next(100, 1000);
 System.Console.WriteLine(num);
-System.Console.WriteLine((num / 100) * 10 + (num % 10));
+System.Console.WriteLine(num / 100 * 10 + num % 10);
 
 // int num = new Random().Next(100, 1000);
 // System.Console.WriteLine(num);
 // string chr = Convert.ToString(num);
 // System.Console.WriteLine($"{chr [0]}{chr [^1]}");
+// знак $ позволяет выводить значения переменных в строке
+// ($"{chr [0]}{chr [^1]}") вместо (chr [0] + "" + chr [^1])
 
 
 // вывод первой и последней буквы слова. ^1 означает array.Length - 1
