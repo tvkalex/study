f859ca1b634e50475880edc507234708eca2c4d4
tvkalex <tvkalex@yandex.ru>
2022-12-03 13:38:09 +0300

    Add Ex017_DivWoRemains solution

--- Ex017_DivWoRemains/Program.cs
+++ Ex017_DivWoRemains/Program.cs
@@ -4,11 +4,22 @@
 // • 34, 5 - > не кратно, остаток 4
 // • 16, 4 -> кратно
 
-System.Console.WriteLine("Введите первое числ: ");
-int num1 = int.Parse(Console.ReadLine()!);
-System.Console.WriteLine("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine()!);
-if(num1 / num2 == num2)
+// System.Console.WriteLine("Введите первое числ: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// System.Console.WriteLine("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// if (num1 / num2 == num2)
+// {
+//     System.Console.WriteLine("кратно");
+// }
+// else
+// {
+//     System.Console.WriteLine($"не кратно, остаток {num1 % num2}");
+// }
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 % num2 == 0)
 {
     System.Console.WriteLine("кратно");
 }
