2542cfdebfd489e506f2df2acb3856b874fb8e6f
tvkalex <tvkalex@yandex.ru>
2022-12-03 13:33:29 +0300

    Add Ex017_DivWoRemains solution

--- Ex017_DivWoRemains/Program.cs
+++ Ex017_DivWoRemains/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+// • 34, 5 - > не кратно, остаток 4
+// • 16, 4 -> кратно
+
+System.Console.WriteLine("Введите первое числ: ");
+int num1 = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if(num1 / num2 == num2)
+{
+    System.Console.WriteLine("кратно");
+}
+else
+{
+    System.Console.WriteLine($"не кратно, остаток {num1 % num2}");
+}
