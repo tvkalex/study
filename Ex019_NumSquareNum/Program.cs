48e82deb322437af1e87c070c0c5235755a092c7
tvkalex <tvkalex@yandex.ru>
2022-12-03 15:35:12 +0300

    Add Ex019_NumSquareNum solution

--- Ex019_NumSquareNum/Program.cs
+++ Ex019_NumSquareNum/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+System.Console.Write("Введите первое число: ");
+int num1 = int.Parse(System.Console.ReadLine()!);
+System.Console.Write("Введите второе число: ");
+int num2 = int.Parse(System.Console.ReadLine()!);
+string result = (num1 * num1 == num2 || num2 * num2 == num1) ? "Да" : "Нет";
+System.Console.WriteLine(result);
