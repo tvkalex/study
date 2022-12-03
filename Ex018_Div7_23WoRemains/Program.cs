e36b0888f348f96ff887150750cb9b340f248a5d
tvkalex <tvkalex@yandex.ru>
2022-12-03 14:54:58 +0300

    Add Ex018_Div7_23WoRemains solution

--- Ex018_Div7_23WoRemains/Program.cs
+++ Ex018_Div7_23WoRemains/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+System.Console.Write("Введите число: ");
+int num = int.Parse(System.Console.ReadLine()!);
+string result = (num % 7 == 0 && num % 23 == 0) ? "Да" : "Нет";
+System.Console.WriteLine(result);
+
