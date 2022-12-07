022052bb9a9b104ae03d4c5a4315d60112193091
tvkalex <tvkalex@yandex.ru>
2022-12-07 18:32:57 +0300

    Add Ex023_SqrtAllFromN solution

--- Ex023_SqrtAllFromN/Program.cs
+++ Ex023_SqrtAllFromN/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход _число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+int i = 1;
+for (i = 1; i <= n; i++)
+{
+    System.Console.Write($"{i * i} ");
+}
