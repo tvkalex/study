055ce50d860e72e829ad9cf7d07b76cd804da04d
tvkalex <tvkalex@yandex.ru>
2022-12-08 20:30:45 +0300

    Add Ex026_Lesson3_ForCycle

--- Ex026_Lesson3_ForCycle/Program.cs
+++ Ex026_Lesson3_ForCycle/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Цикл for
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+//string res = Method4(10, "Метод 4, ");
+//Console.WriteLine(res);
+
+// Цикл в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console. WriteLine();
+}
