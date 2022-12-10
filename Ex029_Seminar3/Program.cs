5021d34603609f1a4e9e8aea10da55d22a61b519
tvkalex <tvkalex@yandex.ru>
2022-12-10 14:13:24 +0300

    Add Ex029_Seminar3 solution

--- Ex029_Seminar3/Program.cs
+++ Ex029_Seminar3/Program.cs
@@ -1,4 +1,30 @@
-﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт сумму чисел от 1 до А.
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+
+int N = InputNumber("Введите число N: ");
+int B = InputNumber("Введите другое: ");
+
+int result = SumA(N);
+int result2 = SumA(B);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {N} = {result}");
+System.Console.WriteLine($"Сумма чисел от 1 до {B} = {result2}");
+
+int InputNumber(string text)
+{
+    System.Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumA(int number)
+{
+    int result = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        result += i; // или result = result + i
+    }
+    return result;
+}
