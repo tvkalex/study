ef12c225268f166fdd39d47a3ba1284a25e9c00f
tvkalex <tvkalex@yandex.ru>
2022-12-14 15:06:52 +0300

    Add Ex032_Seminar5_2 solution

--- Ex032_Seminar5_2/Program.cs
+++ Ex032_Seminar5_2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замена элементов массива: положительные элементы
+// замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] tempArr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < tempArr.Length; i++)
+    {
+        tempArr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return tempArr;
+}
+
+void ChangeNumber(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+const int SIZE = 5;
+const int LEFT_RANGE = -9;
+const int RIGHT_RANGE = 9;
+
+int[] arr = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+System.Console.Write("[" + string.Join(", ", arr) + "]" + " --> ");
+
+ChangeNumber(arr);
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
