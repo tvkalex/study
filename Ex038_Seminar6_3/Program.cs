5f47c0a40a1b743a8cd37b54eb768aad2c76cae4
tvkalex <tvkalex@yandex.ru>
2022-12-17 19:16:24 +0300

    Add Ex038_Seminar6_3 Solution

--- Ex038_Seminar6_3/Program.cs
+++ Ex038_Seminar6_3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int num = inputNumber("Ведите число: ");
+func(num);
+void func(int num)
+{
+    if (num / 2 != 0)
+    func(num / 2);
+    System.Console.Write(num % 2);
+}
+System.Console.WriteLine();
+
+int inputNumber(string message)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(message);
+        text = Console.ReadLine()!;
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
