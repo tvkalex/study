1e476b921e6c37ba2c96019fa92f1ceb6de7e580
tvkalex <tvkalex@yandex.ru>
2022-12-17 22:11:09 +0300

    Add Ex038_Seminar6_3 Solution

--- Ex038_Seminar6_3/Program.cs
+++ Ex038_Seminar6_3/Program.cs
@@ -3,16 +3,39 @@
 // 3 -> 11
 // 2 -> 10
 
-
+// вариант 1
 int num = inputNumber("Ведите число: ");
 func(num);
 void func(int num)
 {
     if (num / 2 != 0)
-    func(num / 2);
+        func(num / 2);
     System.Console.Write(num % 2);
 }
 System.Console.WriteLine();
+
+// вариант 2
+int N = inputNumber("Введите число для конвертации: ");
+GetBinaryView(N);
+System.Console.WriteLine();
+
+void GetBinaryView(int number)
+{
+    if (number <= 0) return;
+    GetBinaryView(number / 2);
+    System.Console.Write(number % 2);
+}
+
+// вариант 3
+int num_2 = inputNumber("Введите десятичное число: ");
+int t = num_2;
+string s = "";
+while (num_2 > 0)
+{
+    s = Convert.ToString(num_2 % 2) + s;
+    num_2 /= 2;
+}
+System.Console.WriteLine($"Десятичное число {t} = {s} в двоичной CC");
 
 int inputNumber(string message)
 {
