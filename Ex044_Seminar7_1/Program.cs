20feb0795817e269ac37c31c378bf8130ca32ec7
tvkalex <tvkalex@yandex.ru>
2022-12-23 16:45:50 +0300

    Add Ex044_Seminar7_1

--- Ex044_Seminar7_1/Program.cs
+++ Ex044_Seminar7_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Аргументы "out"
+
+// Console.Clear();
+// int[] newArr = func(2, 3, 8);
+// System.Console.WriteLine($"{newArr[0]} - {newArr[1]}");
+
+// int[] func(int a, int b, int c)
+// {
+//     int[] arr = new int[2];
+//     arr[0] = a + b;
+//     arr[1] = b + c;
+//     return arr;
+// }
+
+Console.Clear();
+func(out int x, out int y, 2, 3, 8);
+System.Console.WriteLine($"{x} - {y}");
+
+void func(out int x, out int y, int a, int b, int c)
+{
+    x = a + b;
+    y = b + c;
+}
