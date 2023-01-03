a2c00ab39986d9d51599dc96dd5b640afb9f600f
tvkalex <tvkalex@yandex.ru>
2023-01-03 15:37:53 +0300

    Add Ex053_Lection7_Recursion

--- Ex053_Lection7_Recursion/Program.cs
+++ Ex053_Lection7_Recursion/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Собрать строку с числами от a до b, a ≤ b
+
+// string NumbersFor(int a, int b)
+//  {
+//      string result = String.Empty;
+//      for (int i = a; i <= b; i++) // Инкремент счетчика i++, т.к. ищем числа от меньшего к большему
+//      {
+//          result += $"{i} ";
+//      }
+//      return result;
+//  }
+//  string NumbersRec(int a, int b)
+//  {
+//      if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+//      else return String.Empty;
+//  }
+//  Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+//  Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+ // Собрать строку с числами от а до b, a ≥ b
+
+ string NumbersFor(int a, int b)
+ {
+     string result = String.Empty;
+     for (int i = a; i >= b; i--) // Декремент счетчика i--, т.к. ищем числа от большего к меньшему
+     {
+         result += $"{i} ";
+     }
+     return result;
+ }
+ string NumbersRec(int a, int b)
+ {
+     if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+     else return String.Empty;
+ }
+ Console.WriteLine(NumbersFor(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+ Console.WriteLine(NumbersRec(1, 10)); // 10 9 8 7 6 5 4 3 2 1
