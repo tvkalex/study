1b7b41ce0c791e76ceff081debfa70a9f11dcb2b
tvkalex <tvkalex@yandex.ru>
2023-01-03 19:05:32 +0300

    Add Ex000

--- Ex000/Program.cs
+++ Ex000/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i >= b; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+// Console.WriteLine(NumbersFor(10, 1));
+
+ void Towers(string with = "1", string on = "3", string some = "2", int count = 8)
+ {
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
