bd82a4a356e6995f574ebead8d1f029333ec0fae
tvkalex <tvkalex@yandex.ru>
2023-01-03 19:05:07 +0300

    Add Ex053_Lection7_Recursion

--- Ex053_Lection7_Recursion/Program.cs
+++ Ex053_Lection7_Recursion/Program.cs
@@ -1,37 +1,173 @@
 ﻿// Собрать строку с числами от a до b, a ≤ b
 
-// string NumbersFor(int a, int b)
-//  {
-//      string result = String.Empty;
-//      for (int i = a; i <= b; i++) // Инкремент счетчика i++, т.к. ищем числа от меньшего к большему
-//      {
-//          result += $"{i} ";
-//      }
-//      return result;
-//  }
-//  string NumbersRec(int a, int b)
-//  {
-//      if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
-//      else return String.Empty;
-//  }
-//  Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
-//  Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++) // Инкремент счетчика i++, т.к. ищем числа от меньшего к большему
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
 
- // Собрать строку с числами от а до b, a ≥ b
+// Собрать строку с числами от а до b, a ≥ b
 
- string NumbersFor(int a, int b)
- {
-     string result = String.Empty;
-     for (int i = a; i >= b; i--) // Декремент счетчика i--, т.к. ищем числа от большего к меньшему
-     {
-         result += $"{i} ";
-     }
-     return result;
- }
- string NumbersRec(int a, int b)
- {
-     if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
-     else return String.Empty;
- }
- Console.WriteLine(NumbersFor(1, 10)); // 10 9 8 7 6 5 4 3 2 1
- Console.WriteLine(NumbersRec(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+string NumbersFor1(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i >= b; i--) // Декремент счетчика i--, т.к. ищем числа от большего к меньшему
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+string NumbersRec1(int a, int b)
+{
+    if (a >= b) return $"{a} " + NumbersRec1(a - 1, b);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersFor1(10, 1)); // 10 9 8 7 6 5 4 3 2 1
+Console.WriteLine(NumbersRec1(10, 1)); // 10 9 8 7 6 5 4 3 2 1
+
+// Сумма чисел от 1 до n
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumRec(10)); // 55
+
+// Факториал числа
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialFor(10)); // 3628800
+Console.WriteLine(FactorialRec(10)); // 3628800
+
+// Вычислить а в степени n
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+int PowerRec(int a, int n)
+{   //return n == 0 ? 1 : PowerRec(a, n - 1) * a; // Решение в одну строчку с использованием тернарного оператора
+    if (n == 0) return 1;
+    else return PowerRec(a, n - 1) * a;
+}
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
+Console.WriteLine(PowerRecMath(2, 10)); // 1024
+
+// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в».
+// Покажите все слова, состоящие из T букв, которые можно построить из букв этого алфавита
+
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[2]);
+
+// Как посмотреть содержимое папки?
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + "  ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"/Users/tvkalex/Yandex.Disk.localized/8. GeekBrains/GitHub/Examples/Ex000";
+CatalogInfo(path);
+
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo catalog = new DirectoryInfo(path);
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for (int i = 0; i < catalogs.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + "  ");
+//     }
+//     FileInfo[] files = catalog.GetFiles();
+//     for (int i = 0; i < files.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{files[i].Name}");
+//     }
+// }
+// string path = @"/Users/tvkalex/Yandex.Disk.localized/8. GeekBrains/GitHub/Examples/Ex000";
+// CatalogInfo(path);
+
+// Игра в пирамидки
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
+
+// Обход разных структур
+// ((4 - 2) * (1 + 3)) / 10
+string emp = String. Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+//                 0    1    2     3    4    5    6    7    8    9    10   11
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
